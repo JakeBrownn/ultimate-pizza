85baa551065dc45d9070efc2dc4400cad50dc779
Jake <jake@adaptdigital.co>
2018-04-23 08:57:16 +0100

    Add base functionality to game.

--- src/components/App.js
+++ src/components/App.js
@@ -7,51 +7,57 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clicks: 100,
+      clicks: 0,
       shopAutoClicksPrice: 10,
       shopAutoClicksBrought: 1,
       shopBonusClicksPrice: 25,
       shopBonusClicksBrought: 1
     }
-  }
+  };
   
-  buttonClicked = () => {
-    if (this.state.shopBonusClicksBrought > 1) {
-      this.setState({ clicks: this.state.clicks + this.state.shopBonusClicksBrought})
+  imageClicked = () => {
+    const { shopBonusClicksBrought, clicks } = this.state;
+
+    if (shopBonusClicksBrought > 1) {
+      this.setState({ clicks: clicks + shopBonusClicksBrought})
     } else {
-      this.setState({ clicks: this.state.clicks + 1 })
+      this.setState({ clicks: clicks + 1 })
     }
   };
 
   autoClicks = () => {
-    if (this.state.clicks >= this.state.shopAutoClicksPrice) {
+    const { clicks, shopAutoClicksPrice, shopAutoClicksBrought } = this.state;
+
+    if (clicks >= shopAutoClicksPrice) {
       this.setState({ 
-        clicks: this.state.clicks - this.state.shopAutoClicksPrice,
-        shopAutoClicksBrought: this.state.shopAutoClicksBrought + 1,
+        clicks: clicks - shopAutoClicksPrice,
+        shopAutoClicksBrought: shopAutoClicksBrought + 1,
       });
 
-      if (this.state.shopAutoClicksBrought > 3) {
-        this.setState({ shopAutoClicksPrice: this.state.shopAutoClicksPrice * 2 });
-      } else if (this.state.shopAutoClicksBrought > 10) {
-        this.setState({ shopAutoClicksPrice: this.state.shopAutoClicksPrice * 2.5 });
+      if (shopAutoClicksBrought > 3) {
+        this.setState({ shopAutoClicksPrice: shopAutoClicksPrice * 2 });
+      } else if (shopAutoClicksBrought > 10) {
+        this.setState({ shopAutoClicksPrice: shopAutoClicksPrice * 2.5 });
       } else {
-        this.setState({ shopAutoClicksPrice: this.state.shopAutoClicksPrice * 1.5 });
+        this.setState({ shopAutoClicksPrice: shopAutoClicksPrice * 1.5 });
       }
 
       setInterval(() => this.setState({
-        clicks: this.state.clicks + this.state.shopAutoClicksBrought
+        clicks: clicks + shopAutoClicksBrought
       }), 10000);
     }
   };
 
   bonusClicks = () => {
-    if (this.state.clicks >= this.state.shopBonusClicksPrice) {
+    const { clicks, shopBonusClicksPrice, shopBonusClicksBrought } = this.state;
+
+    if (clicks >= shopBonusClicksPrice) {
       this.setState({
-        clicks: this.state.clicks - this.state.shopBonusClicksPrice,
-        shopBonusClicksBrought: this.state.shopBonusClicksBrought + 1
+        clicks: clicks - shopBonusClicksPrice,
+        shopBonusClicksBrought: shopBonusClicksBrought + 1
       });
 
-      this.setState({ shopBonusClicksPrice: this.state.shopBonusClicksPrice * 2 });
+      this.setState({ shopBonusClicksPrice: shopBonusClicksPrice * 2 });
     }
   };
 
@@ -60,29 +66,38 @@ class App extends Component {
       <div className="site-content">
         <div className="game-screen">
           <div className="action-button">
-            <img className="action-button__image" onClick={this.buttonClicked} src={pizza} alt="Pizza Image" />
+            <img className="action-button__image" onClick={this.imageClicked} src={pizza} alt="Pizza" />
           </div>
           <div className="user-score">
-            <span className="user-score__value">{this.state.clicks}</span>
+            <span className="user-score__value">{this.state.clicks} Slices</span>
           </div>
         </div>
 
         <div className="shop">
+          <h2 className="shop__title">Upgrades</h2>
           <div className="shop-item">
             <h2 className="shop-item__title">Delivery Man</h2>
-            <p className="shop-item__desc">Description: Adds {this.state.shopAutoClicksBrought} clicks every 10 seconds</p>
-            <p className="shop-item__desc">Next Upgrade: Adds {this.state.shopAutoClicksBrought + 1} clicks every 10 seconds</p>
+            <div className="shop-item__popup">
+              <p className="shop-item__owned">[Owned: {this.state.shopAutoClicksBrought - 1}]</p>
+              <p className="shop-item__desc">Adds {this.state.shopAutoClicksBrought} clicks every 10 seconds</p>
+              <ul className="shop-item__upgrade-list">
+                <li className="shop-item__upgrade-text">Adds {this.state.shopAutoClicksBrought + 1} clicks every 10 seconds</li>
+              </ul>
+            </div>
             <p className="shop-item__cost">Cost: {this.state.shopAutoClicksPrice}</p>
-            <p className="shop-item__owned">Owned: {this.state.shopAutoClicksBrought - 1}</p>
-            <button onClick={this.autoClicks}>Purchase</button>
+            <button className="shop-item__button" onClick={this.autoClicks}>Purchase</button>
           </div>
           <div className="shop-item">
             <h2 className="shop-item__title">More Cheese</h2>
-            <p className="shop-item__desc">Description: Clicks are now worth {this.state.shopBonusClicksBrought} </p>
-            <p className="shop-item__desc">Next Upgrade: Clicks are now worth {this.state.shopBonusClicksBrought * 1.5} </p>
+            <div className="shop-item__popup">
+              <p className="shop-item__owned">[Owned: {this.state.shopBonusClicksBrought - 1}]</p>
+              <p className="shop-item__desc">Clicks are now worth {this.state.shopBonusClicksBrought} </p>
+              <ul className="shop-item__upgrade-list">
+                <li className="shop-item__upgrade-text">Clicks are now worth {this.state.shopBonusClicksBrought * 1.5} </li>
+              </ul>
+            </div>
             <p className="shop-item__cost">Cost: {this.state.shopBonusClicksPrice}</p>
-            <p className="shop-item__owned">Owned: {this.state.shopBonusClicksBrought - 1}</p>
-            <button onClick={this.bonusClicks}>Purchase</button>
+            <button className="shop-item__button" onClick={this.bonusClicks}>Purchase</button>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-import Header from './components/Header';
 import App from './components/App';
 
-const Homepage = () => (
-  <div>This is my Homepage</div>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div className="browser-router">
-      {/* <Header /> */}
-      <Route exact path="/" component={App} />
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('root-app'));
+ReactDOM.render(<App />, document.getElementById('root-app'));
 registerServiceWorker();
