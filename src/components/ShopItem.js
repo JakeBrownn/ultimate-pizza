ee71bb998cbe0164ae6605616077dcd107694436
Jake <jake@adaptdigital.co>
2018-05-22 09:11:40 +0100

    Remove .vscode and audio files from git repo.

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const ShopItem = (props) => {
   const { triggerAction, icon, title, purchased, description, itemCost } = props;
